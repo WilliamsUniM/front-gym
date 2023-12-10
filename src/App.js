b9b59f695a00496b65611b52fce782c2b22a9efe
Williams Maldonado <50841358+WilliamsMaldonado@users.noreply.github.com>
2023-12-10 16:49:41 -0500

    front finish

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import React, { useState } from 'react';
 import './App.css';
 import Home from './components/home';
 import Login from './components/login';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Usuarios from './components/usuarios';
+import Pagos from './components/pagos';
+import Reportes from './components/reportes';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [token, setToken] = useState();
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
 
-  if (!token) {
-    return <Login setToken={setToken} />
+  if (!loggedIn) {
+    return <Login setLoggedIn={setLoggedIn} setEmail={setEmail} />
   }
 
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/usuarios" element={<Usuarios />} />
+      <Route exact path="/pagos" element={<Pagos />} />
+      <Route exact path="/reportes" element={<Reportes />} />
+    </Routes>
   );
 }
 
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,23 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import './login.css';
 
-export default function Login() {
+export default function Login(props) {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+    const navigate = useNavigate();
+
+    const onButtonClick = () => {
+        setError("")
+
+        if ("" === email) {
+            setError("Por favor ingrese su email")
+            return
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            setError("ingrese un email valido")
+            return
+        }
+
+        if ("" === password) {
+            setError("Por favor ingrese su password")
+            return
+        }
+
+        if (password.length < 6) {
+            setError("El password debe ser mayor a 6 caracteres")
+            return
+        }
+
+        logIn()
+    }
+
+    const logIn = () => {
+        fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: email, password: password}),
+        })
+        .then(r => r.json())
+        .then(r => {
+            if (r.token) {
+                localStorage.setItem("user", JSON.stringify({email, token: r.token}))
+                props.setLoggedIn(true)
+                props.setEmail(email)
+                navigate('/home', {replace: true})
+            } else {
+                setError("email o password invalidos")
+            }
+        })
+        .catch(err => {
+            console.log("ERROR: ", err)
+        })
+    }
+
     return (
         <div className="login-wrapper">
-            <img src={`${process.env.PUBLIC_URL}/gym.png`} style={{width: 10 + '%'}}/>
-            <form>
-                <label>
-                    <p>USUARIO</p>
-                    <input type="text" className="input-user" placeholder='USUARIO'/>
-                </label>
-                <label>
-                    <p>CONTRASEÑA</p>
-                    <input type="password" className="input-password" placeholder='CONTRASEÑA' style={{backgroundImage:`${process.env.PUBLIC_URL}/iconuser.png`}}/>
-                </label>
-                <div>
-                    <button type="submit" >INICIAR SESIÓN</button>
-                </div>
-            </form>
+            <img src={`${process.env.PUBLIC_URL}/gym.png`} style={{ width: 10 + '%' }} />
+            <label>
+                <p>USUARIO</p>
+                <input value={email} type="text" className="input-user" placeholder='EMAIL' onChange={ev => setEmail(ev.target.value)} />
+            </label>
+            <label>
+                <p>CONTRASEÑA</p>
+                <input value={password} type="password" className="input-password" placeholder='CONTRASEÑA' onChange={ev => setPassword(ev.target.value)}
+                    style={{ backgroundImage: `${process.env.PUBLIC_URL}/iconuser.png` }} />
+            </label>
+            <div>
+                <button className="button" onClick={onButtonClick}>INICIAR SESIÓN</button>
+            </div>
+            <label><p>{error}</p></label>
         </div>
     )
 }
